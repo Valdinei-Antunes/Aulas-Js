fce344ef57504a99ec6e74b7ae2a291c66c342d4
Valdineii <valdinei.filho@a.ucb.br>
2025-05-14 20:50:30 -0300

    Ajustando o arquivo

---
+++ Basico/js/tipos.js
@@ -0,0 +1,9 @@
+let numero = 2; //numero
+let numero2 = 2.4; // numero com ponto flutuante
+const pi = 3.14; //constante numero com ponto flutuante
+let nome = "Valdinei"; //String
+let verdadeiro = false; //booleno
+
+let resultado = numero + numero2;
+
+console.log(resultado);
