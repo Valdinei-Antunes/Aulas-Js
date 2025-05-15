a532033e264b49290337b377aca448507fcdac54
Valdineii <valdinei.filho@a.ucb.br>
2025-05-14 21:33:38 -0300

    Variavies

---
+++ Basico/js/variaveis.js
@@ -0,0 +1,24 @@
+// Var
+
+var primeironome, sobrenome, nomeCompleto, idade, soma;
+
+primeironome = "Valdinei";
+sobrenome= "Antunes";
+idade = 30;
+nomeCompleto = nome + " " + sobrenome;
+
+soma = idade + 10;
+pessoa = nome + " " + soma;
+
+document.getElementById("texto").innerHTML = pessoa;
+
+
+//let, a variavel não pode ser redeclarada
+
+let op = 14419;
+
+//const
+
+const pi = 3.14
+
+alert(pi);
